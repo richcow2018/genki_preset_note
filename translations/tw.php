401d962499c4e0106b06ca05c593e0cfa3c454e2
Oliver Mak <oliver139.working@gmail.com>
2022-06-02 14:06:46 +0800

    Add translations

---
+++ translations/tw.php
@@ -0,0 +1,18 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_c2bc20bf729927e5310ab7aaf060a070'] = '備註內容';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_4d3d769b812b6faa6b76e1a8abaece2d'] = '啟用';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_d3b206d196cd6be3a2764c1fb90b200f'] = '刪除選擇的';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_e25f0ecd41211b01c83e5fec41df4fe7'] = '刪除選擇的東西？';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_2fecca5778b0e7d8ce4505918fb4e0ac'] = '新增備註';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_5528c7c71dc518039d0a00f0da863c25'] = '建立新備註';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_edd7b6125f0548965c21efa9c2bcc85e'] = '編輯備註';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_3b0649c72650c313a357338dcdfb64ec'] = '備註';
+$_MODULE['<{genki_preset_note}prestashop>adminpresetnote_c9cc8cce247e49bae79f15173ce97354'] = '儲存';
+$_MODULE['<{genki_preset_note}prestashop>preset_note_box_b0b7934f6eb1a31b3f0954e5336fa0f7'] = '訂單備註';
+$_MODULE['<{genki_preset_note}prestashop>preset_note_box_b962015f3959dc95d7b80a17c5104838'] = '從已設置的備註中選擇';
+$_MODULE['<{genki_preset_note}prestashop>preset_note_box_a5a442dd549b35d6d73c12a7b6456cc3'] = '設置備註';
+$_MODULE['<{genki_preset_note}prestashop>preset_note_box_3b0649c72650c313a357338dcdfb64ec'] = '備註';
+$_MODULE['<{genki_preset_note}prestashop>preset_note_box_c9cc8cce247e49bae79f15173ce97354'] = '儲存';
